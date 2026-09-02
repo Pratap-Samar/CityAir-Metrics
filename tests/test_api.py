e99845ecfccd9cc0506c87dc7bebe3682f9508b7
Pratap-Samar <samarpratap100904@gmail.com>
2026-09-03 00:08:38 +0530

    Make cities API test self-contained

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,12 +1,31 @@
 from fastapi.testclient import TestClient
 
 from api.main import app
+from database.connection import get_connection
+from database.repositories import get_or_create_city
 
 
 client = TestClient(app)
 
 
 def test_get_cities():
+    connection = get_connection()
+
+    try:
+        city = {
+            "name": "Test API City",
+            "country": "Test API Country",
+            "latitude": 11.1234,
+            "longitude": 21.5678,
+        }
+
+        get_or_create_city(
+            city,
+            connection,
+        )
+    finally:
+        connection.close()
+
     response = client.get("/cities")
 
     assert response.status_code == 200
@@ -14,14 +33,13 @@ def test_get_cities():
     cities = response.json()
 
     assert isinstance(cities, list)
-    assert cities
 
     test_city = next(
         city
         for city in cities
-        if city["name"] == "Test City"
-        and city["country"] == "Test Country"
+        if city["name"] == "Test API City"
+        and city["country"] == "Test API Country"
     )
 
-    assert test_city["latitude"] == 10.1234
-    assert test_city["longitude"] == 20.5678
+    assert test_city["latitude"] == 11.1234
+    assert test_city["longitude"] == 21.5678
